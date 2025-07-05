1b5161ac84cead6974bffb8b741525533f5a990f
n55t <nsst@ymail.ne.jp>
2025-07-06 02:23:31 +0900

    🗃️モデルを定義

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+
+class CustomUser(AbstractUser):
+    address = models.CharField("住所", max_length=255, blank=True)
+    birth_date = models.DateField("生年月日", null=True, blank=True)
+    phone = models.CharField("電話番号", max_length=20, blank=True)
+    postal_code = models.CharField("郵便番号", max_length=10, blank=True)
+
+    def __str__(self):
+        return self.username
