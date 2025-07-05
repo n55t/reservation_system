961320ceac647483c2c6cf723051b0704f119db3
n55t <nsst@ymail.ne.jp>
2025-07-06 03:38:03 +0900

    💄ログイン画面を作成

--- config/urls.py
+++ config/urls.py
@@ -17,6 +17,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from accounts.views import CustomLoginView
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('login/', CustomLoginView.as_view(), name='login'),
 ]
