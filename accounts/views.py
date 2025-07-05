50a14808c2939c84c25d68173dab1101d0dbfaa6
n55t <nsst@ymail.ne.jp>
2025-07-06 08:16:47 +0900

    💄ログイン画面のUIを更新

--- accounts/views.py
+++ accounts/views.py
@@ -1,5 +1,12 @@
+from django.contrib.auth.forms import AuthenticationForm
 from django.contrib.auth.views import LoginView
 
 
 class CustomLoginView(LoginView):
     template_name = 'login.html'
+
+    def get_form(self, form_class=None):
+        form = super().get_form(form_class)
+        form.fields['username'].widget.attrs.update({'class': 'form-control', 'placeholder': 'ユーザー名'})
+        form.fields['password'].widget.attrs.update({'class': 'form-control', 'placeholder': 'パスワード'})
+        return form
