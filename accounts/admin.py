fa506f19ac02cde48caef9edb17b099086d2bc07
n55t <nsst@ymail.ne.jp>
2025-07-06 02:51:32 +0900

    🔧機能を設定

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 
-# Register your models here.
+from .models import CustomUser
+
+
+class CustomUserAdmin(UserAdmin):
+    model = CustomUser
+    fieldsets = UserAdmin.fieldsets + (
+        ("追加情報", {
+            "fields": ("address", "birth_date", "phone", "postal_code")
+        }),
+    )
+
+admin.site.register(CustomUser, CustomUserAdmin)
