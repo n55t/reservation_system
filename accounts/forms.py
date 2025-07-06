c47ab0b20d0ac8981e18d96bd47f27b5813efb09
n55t <nsst@ymail.ne.jp>
2025-07-06 16:11:17 +0900

    💄ユーザー登録画面を改善

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,24 +1,51 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from .models import CustomUser
+import re
+from datetime import date
 
 class CustomUserCreationForm(UserCreationForm):
     class Meta(UserCreationForm.Meta):
         model = CustomUser
-        fields = ('username', 'email', 'address', 'birth_date', 'phone', 'postal_code')
-        labels = {
-            'username': 'ユーザー名',
-            'email': 'メールアドレス',
-            'address': '住所',
-            'birth_date': '生年月日',
-            'phone': '電話番号',
-            'postal_code': '郵便番号',
-            'password2': 'パスワード（確認用）',
-        }
+        fields = (
+            'last_name', 'first_name', 'last_name_kana', 'first_name_kana',
+            'username', 'email', 'birth_date', 'phone',
+            'postal_code', 'prefecture', 'city', 'street_address', 'building_name'
+        )
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        self.fields['email'].required = True
-        for field in self.fields.values():
+        # フィールドのラベルを日本語に設定
+        self.fields['last_name'].label = "姓"
+        self.fields['first_name'].label = "名"
+        self.fields['last_name_kana'].label = "姓（かな）"
+        self.fields['first_name_kana'].label = "名（かな）"
+        self.fields['username'].label = "ユーザー名"
+        self.fields['email'].label = "メールアドレス"
+        self.fields['birth_date'].label = "生年月日"
+        self.fields['phone'].label = "電話番号"
+        self.fields['postal_code'].label = "郵便番号"
+        self.fields['prefecture'].label = "都道府県"
+        self.fields['city'].label = "市区町村"
+        self.fields['street_address'].label = "番地など"
+        self.fields['building_name'].label = "建物名・部屋番号"
+
+        # 生年月日のウィジェットをSelectDateWidgetに変更
+        years = range(date.today().year - 100, date.today().year + 1)
+        self.fields['birth_date'].widget = forms.SelectDateWidget(years=years)
+
+        # プレースホルダーとCSSクラスの設定
+        for field_name, field in self.fields.items():
             field.widget.attrs['class'] = 'form-control'
-            field.widget.attrs['placeholder'] = field.label
+            if field_name not in ['birth_date']:
+                field.widget.attrs['placeholder'] = field.label
+
+        # 電話番号のヘルプテキスト
+        self.fields['phone'].help_text = "ハイフンなしで入力してください。"
+
+    def clean_phone(self):
+        phone = self.cleaned_data.get('phone')
+        if phone:
+            # 数字以外の文字をすべて削除
+            return re.sub(r'\D', '', phone)
+        return phone
--- accounts/models.py
+++ accounts/models.py
@@ -3,10 +3,22 @@ from django.contrib.auth.models import AbstractUser
 
 
 class CustomUser(AbstractUser):
-    address = models.CharField("住所", max_length=255, blank=True)
-    birth_date = models.DateField("生年月日", null=True, blank=True)
+    # 氏名（かな）
+    last_name_kana = models.CharField("姓（かな）", max_length=150, blank=True)
+    first_name_kana = models.CharField("名（かな）", max_length=150, blank=True)
+
+    # 電話番号
     phone = models.CharField("電話番号", max_length=20, blank=True)
-    postal_code = models.CharField("郵便番号", max_length=10, blank=True)
+
+    # 生年月日
+    birth_date = models.DateField("生年月日", null=True, blank=True)
+
+    # 住所
+    postal_code = models.CharField("郵便番号", max_length=7, blank=True)
+    prefecture = models.CharField("都道府県", max_length=40, blank=True)
+    city = models.CharField("市区町村", max_length=100, blank=True)
+    street_address = models.CharField("番地など", max_length=255, blank=True)
+    building_name = models.CharField("建物名・部屋番号", max_length=255, blank=True)
 
     def __str__(self):
         return self.username
